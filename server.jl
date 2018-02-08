811f582b5e8aa114e0f11b6525e15edb62030d4e
Robin Deits <robin.deits@gmail.com>
2018-02-08 14:21:48 -0500

    allow multiple clients to connect to the same viewer

--- server.jl
+++ server.jl
@@ -35,8 +35,27 @@ extcode(::Type{Float32}) = 0x17
 
 MsgPack.pack(io::IO, v::PackedVector) = pack(io, Ext(extcode(v), reinterpret(UInt8, v.data, (sizeof(v.data),))))
 
-wsh = WebSocketHandler() do req,client
-    while true
+function serve_geometry()
+    my_port = 5005 + rand(1:500)
+    magic_port = 8765
+
+    # connection_channel = Channel{Bool}(1)
+    # name_server = Server(WebSocketHandler() do req, client
+    #     write(client, MsgPack.pack(my_port))
+    #     println("got name server connection")
+    #     close(name_server)
+    # end)
+
+    # name_server = Server(HttpHandler() do req::Request, res::Response
+    #     @show my_port
+    #     response = Response("ws://127.0.0.1:$my_port")
+    #     response.headers["Access-Control-Allow-Origin"] = "*"
+    #     close(name_server)
+    #     response
+    # end)
+
+    wsh = WebSocketHandler() do req,client
+        @show client
         req = read(client)
         mesh = load("head_multisense.obj", GLUVMesh)
         texture_png = open(read, "HeadTextureMultisense.png", "r")
@@ -47,14 +66,14 @@ wsh = WebSocketHandler() do req,client
                 "setgeometry" => [
                     Dict(
                         "type" => "pointcloud",
-                        "points" => PackedVector(verts),
+                        "points" => PackedVector(verts .+ [rand(Point3f0)]),
                         "channels" => Dict(
                             "rgb" => PackedVector(verts)
                         )
                     ),
                     Dict(
                         "type" => "mesh_data",
-                        "vertices" => PackedVector(vertices(mesh)),
+                        "vertices" => PackedVector(vertices(mesh) .+ [rand(Point3f0)]),
                         "faces" => PackedVector(faces(mesh)),
                         "texture" => Dict(
                             "coordinates" => PackedVector(texturecoordinates(mesh)),
@@ -65,8 +84,32 @@ wsh = WebSocketHandler() do req,client
 			)
 		)
 		write(client, msg)
+        close(server)
     end
-  end
 
-server = Server(wsh)
-run(server,8765)
+
+    server = Server(wsh)
+    println("running geometry server")
+    @async run(server,my_port)
+
+    @async begin
+        while true
+            try
+                name_server = listen(magic_port)
+                socket = accept(name_server)
+                println("advertising my port as $my_port")
+                response = Response("ws://127.0.0.1:$my_port")
+                response.headers["Access-Control-Allow-Origin"] = "*"
+                write(socket, response)
+                close(socket)
+                close(name_server)
+                break
+            catch e
+                println("waiting")
+                sleep(0.1)
+            end
+        end
+    end
+end
+
+serve_geometry()
