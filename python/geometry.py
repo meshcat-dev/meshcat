436eb153afc8afe5512d923530f31b3cdd104530
Robin Deits <robin.deits@gmail.com>
2018-02-10 23:57:10 -0500

    add pointclouds and turn off gui (because it causes a memory leak)

--- python/geometry.py
+++ python/geometry.py
@@ -1,5 +1,7 @@
 import uuid
+
 import umsgpack
+import numpy as np
 
 
 class Box:
@@ -85,6 +87,91 @@ class SetTransform:
         }
 
 
+def item_size(array):
+    if array.ndim == 1:
+        return 1
+    elif array.ndim == 2:
+        return array.shape[0]
+    else:
+        raise ValueError("I can only pack 1- or 2-dimensional numpy arrays, but this one has {:d} dimensions".format(array.ndim))
+
+
+def threejs_type(dtype):
+    if dtype == np.uint8:
+        return "Uint8Array", 0x12
+    elif dtype == np.int32:
+        return "Int32Array", 0x15
+    elif dtype == np.uint32:
+        return "Uint32Array", 0x16
+    elif dtype == np.float32:
+        return "Float32Array", 0x17
+    else:
+        raise ValueError("Unsupported datatype: " + str(dtype))
+
+
+def pack_numpy_array(x):
+    typename, extcode = threejs_type(x.dtype)
+    return {
+        "itemSize": item_size(x),
+        "type": typename,
+        "array": umsgpack.Ext(extcode, x.tobytes()),
+        "normalized": False
+    }
+
+
+class PointsGeometry:
+    def __init__(self, points, color=None):
+        self.points = points
+        self.color = color
+        self.uuid = str(uuid.uuid1())
+
+    def serialize(self):
+        attrs = {"position": pack_numpy_array(self.points)}
+        if self.color is not None:
+            attrs["color"] = pack_numpy_array(self.color)
+        return {
+            "uuid": self.uuid,
+            "type": "BufferGeometry",
+            "data": {
+                "attributes": attrs
+            }
+        }
+
+
+class PointsMaterial:
+    def __init__(self, size=0.001, color=0xffffff):
+        self.size = size
+        self.color = color
+        self.uuid = str(uuid.uuid1())
+
+    def serialize(self):
+        return {
+            "uuid": self.uuid,
+            "type": "PointsMaterial",
+            "color": self.color,
+            "size": self.size,
+            "vertexColors": 2
+        }
+
+
+class Points:
+    def __init__(self, geometry, material):
+        self.geometry = geometry
+        self.material = material
+
+    def serialize(self):
+        return {
+            "metadata": {"version": 4.5, "type": "Object"},
+            "geometries": [self.geometry.serialize()],
+            "materials": [self.material.serialize()],
+            "object": {
+                "type": "Points",
+                "geometry": self.geometry.uuid,
+                "material": self.material.uuid
+            }
+        }
+
+
 class ViewerMessage:
     def __init__(self, commands):
         self.commands = commands
@@ -94,4 +181,5 @@ class ViewerMessage:
             "commands": [c.serialize() for c in self.commands]
         }
 
-
+    def pack(self):
+        return umsgpack.packb(self.serialize())
--- python/interface.py
+++ python/interface.py
@@ -1,12 +1,12 @@
 import threading
 import random
+import uuid
 
-import umsgpack
+import numpy as np
 
-import geometry
 import server
 import window
-from geometry import ViewerMessage, Mesh, Box, MeshBasicMaterial, SetObject, SetTransform
+from geometry import ViewerMessage, Mesh, Box, MeshBasicMaterial, SetObject, SetTransform, Points, PointsGeometry, PointsMaterial
 
 
 if __name__ == '__main__':
@@ -14,27 +14,39 @@ if __name__ == '__main__':
     window.launch_thread(socket_port)
     manager = server.WebsocketManager(socket_port)
 
-    for i in range(100):
+    with open("../head_multisense.obj", "r") as f:
+        mesh_data = f.read()
+
+    while True:
         path = "foo/bar"
         # if i == 0:
         #     path = "foo/bar"
         # else:
         #     path = input("path: ")
+        verts = np.random.random((3, 100000)).astype(np.float32)
         msg = ViewerMessage([
             SetObject(
                 Mesh(
                     Box([0.2, 0.1, 0.2]),
                     MeshBasicMaterial(0xff00ff)
                 ),
-                path.split("/")
+                path.split("/") + ["box"]
             ),
-            SetTransform(
-                [random.random() for i in range(3)],
-                [0, 0, 0, 1],
-                path.split("/")
-            )
+            SetObject(
+                Points(
+                    PointsGeometry(verts, verts),
+                    PointsMaterial()
+                ),
+                path.split("/") + ["points"]
+            ),
+            # SetTransform(
+            #     [random.random() for i in range(3)],
+            #     [0, 0, 0, 1],
+            #     path.split("/")
+            # )
         ])
-        manager.send_to_all(umsgpack.packb(msg.serialize()))
+        manager.send_to_all(msg.pack())
+        # manager.send_to_all(umsgpack.packb(msg.serialize()))
 
         import time
-        time.sleep(2)
+        time.sleep(0.1)
