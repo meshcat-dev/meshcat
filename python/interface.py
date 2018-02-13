fa9b5a7f356c975c1f6e54465211d128bf920e54
Robin Deits <robin.deits@gmail.com>
2018-02-12 19:37:32 -0500

    cleanup

--- python/interface.py
+++ python/interface.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
             ),
             g.SetObject(
                 g.Points(
-                    g.PointsGeometry(verts, verts),
+                    g.PointsGeometry(verts, color=verts),
                     g.PointsMaterial()
                 ),
                 ["primitives", "points"]
