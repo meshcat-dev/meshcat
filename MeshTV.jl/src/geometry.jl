426d68a7dafe6cc230fafcc15381a6e4a9c4e7f8
Robin Deits <robin.deits@gmail.com>
2018-02-13 23:52:28 -0500

    handle intrinsic tform in julia

--- MeshTV.jl/src/geometry.jl
+++ MeshTV.jl/src/geometry.jl
@@ -5,6 +5,9 @@ using Parameters: @with_kw
 using Base.Random: UUID, uuid1
 using Colors
 using MsgPack
+using CoordinateTransformations
+
+import Base: length
 
 export Mesh,
        MeshBasicMaterial,
@@ -41,12 +44,55 @@ threejstype(m::MeshMaterial) = m._type
 threejstype(o::Mesh) = "Mesh"
 
 
+##### Taken from DrakeVisualizer/geometry_types.jl #####
+# GeometryTypes doesn't define an Ellipsoid type yet, so we'll make one ourselves!
+mutable struct HyperEllipsoid{N, T} <: GeometryPrimitive{N, T}
+    center::Point{N, T}
+    radii::Vec{N, T}
+end
+
+origin(geometry::HyperEllipsoid{N, T}) where {N, T} = geometry.center
+radii(geometry::HyperEllipsoid{N, T}) where {N, T} = geometry.radii
+center(geometry::HyperEllipsoid) = origin(geometry)
+
+mutable struct HyperCylinder{N, T} <: GeometryPrimitive{N, T}
+    length::T # along last axis
+    radius::T
+    # origin is at center
+end
+
+length(geometry::HyperCylinder) = geometry.length
+radius(geometry::HyperCylinder) = geometry.radius
+origin(geometry::HyperCylinder{N, T}) where {N, T} = zeros(SVector{N, T})
+center(g::HyperCylinder) = origin(g)
+
+center(geometry::HyperRectangle) = minimum(geometry) + 0.5 * widths(geometry)
+center(geometry::HyperCube) = minimum(geometry) + 0.5 * widths(geometry)
+center(geometry::HyperSphere) = origin(geometry)
+############################################################
+
+
+intrinsic_transform(g) = IdentityTransformation()
+intrinsic_transform(g::HyperRectangle) = Translation(center(g)...)
+intrinsic_transform(g::HyperSphere) = Translation(center(g)...)
+intrinsic_transform(g::HyperEllipsoid) = Translation(center(g)...)
+intrinsic_transform(g::HyperCylinder) = Translation(center(g)...)
+intrinsic_transform(g::HyperCube) = Translation(center(g)...)
+
+
+function serialize(t::Transformation)
+    H = [transform_deriv(t, Vec(0., 0, 0)) t(Vec(0., 0, 0));
+     Vec(0, 0, 0, 1)']
+    reshape(H, length(H))
+end
+
 function serialize(obj::AbstractObject, uuid=uuid1())
 	data = Dict{String, Any}(
 	    "metadata" => Dict{String, Any}("version" => 4.5, "type" => "Object"),
         "object" => Dict{String, Any}(
             "uuid" => string(uuid),
             "type" => threejstype(obj),
+            "matrix" => serialize(intrinsic_transform(geometry(obj)))
         )
     )
     add_to_object!(data, geometry(obj))
