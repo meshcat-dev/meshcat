55777d65b7baafaf58c89b7406ae5516bf8715ad
Robin Deits <robin.deits@gmail.com>
2018-02-19 16:33:55 -0500

    strip out bindings code

--- archive/prototype/receiver.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import websockets
-
-async def hello():
-    async with websockets.connect('ws://localhost:8764') as websocket:
-        greeting = await websocket.recv()
-        print("< {}".format(greeting))
-
-asyncio.get_event_loop().run_until_complete(hello())
--- meshtv/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import commands
-from . import geometry
-from . import visualizer
-from . import servers
-
-from .visualizer import Visualizer
