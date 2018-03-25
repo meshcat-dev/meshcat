f4da4d3df0927b134d1c8a0d9a4ae56656c9cd47
Robin Deits <robin.deits@gmail.com>
2018-03-25 00:55:26 -0400

    generate minified file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
-module.exports = {
+module.exports = [{
 	entry: './src/index.js',
 	output: {
 		library: "MeshCat",
 		libraryTarget: 'umd'
 	},
 	watch: true,
+	mode: "development",
 	devtool: "cheap-eval-source-map"
-};
+}, {
+	entry: './src/index.js',
+	output: {
+		filename: "main.min.js",
+		library: "MeshCat",
+		libraryTarget: 'umd'
+	},
+	watch: true,
+	mode: "production"
+}];
